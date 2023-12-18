154eb84c86614d327034a40b8c955d4325cde234
Naem-Haq <23379243@studentmail.ul.ie>
2023-12-18 16:00:24 +0000

    Data structures committed + JSON

--- src/main/java/com/ise/epic/DataStructures/HashMapImplementation.java
+++ src/main/java/com/ise/epic/DataStructures/HashMapImplementation.java
@@ -1,15 +1,22 @@
-package com.ise.epic.Map;
+package com.ise.epic.DataStructures;
 
 public class HashMapImplementation<K, V> {
     private Entry<K, V>[] table;
     private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR_THRESHOLD = 0.75f;
+    private int size;
 
     public HashMapImplementation() {
         table = new Entry[INITIAL_CAPACITY];
+        size = 0;
     }
 
     public void put(K key, V value) {
-        int hash = key.hashCode() % INITIAL_CAPACITY;
+        if (shouldResize()) {
+            resize();
+        }
+
+        int hash = key.hashCode() % table.length;
         Entry<K, V> newEntry = new Entry<>(key, value, null);
 
         if (table[hash] == null) {
@@ -21,10 +28,12 @@ public class HashMapImplementation<K, V> {
             }
             current.next = newEntry;
         }
+
+        size++;
     }
 
     public V get(K key) {
-        int hash = key.hashCode() % INITIAL_CAPACITY;
+        int hash = key.hashCode() % table.length;
         if (table[hash] == null) {
             return null;
         } else {
@@ -37,6 +46,24 @@ public class HashMapImplementation<K, V> {
             }
             return null;
         }
+    }
+
+    private boolean shouldResize() {
+        return (float) size / table.length >= LOAD_FACTOR_THRESHOLD;
+    }
+
+    private void resize() {
+        Entry<K, V>[] newTable = new Entry[table.length * 2];
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                Entry<K, V> next = entry.next;
+                int hash = entry.key.hashCode() % newTable.length;
+                entry.next = newTable[hash];
+                newTable[hash] = entry;
+                entry = next;
+            }
+        }
+        table = newTable;
     }
 
     private static class Entry<K, V> {
